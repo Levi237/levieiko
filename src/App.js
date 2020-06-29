e02de1400aa1e1f5fd8291140439bc3b1915b71d
Levi Winans <levieiko@gmail.com>
2020-06-29 10:00:53 -0400

    set up App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render (){
+    return (
+      <AppContainer>
+        Hello World
+      </AppContainer>
+    );
+  }
 }
 
-export default App;
+const AppContainer = styled.div`
+background-color: pink;
+`;
