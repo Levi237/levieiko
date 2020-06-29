377de07a065da3f7bf52173e99256a9f00a88d89
Levi Winans <levieiko@gmail.com>
2020-06-29 10:40:52 -0400

    add hero image

--- src/App.js
+++ src/App.js
@@ -7,12 +7,31 @@ export default class App extends Component {
   render (){
     return (
       <AppContainer>
+        <HeaderImage>
+        </HeaderImage>
+        <BodyContainer>
+
         Hello World
+        </BodyContainer>
       </AppContainer>
     );
   }
 }
+const BodyContainer = styled.div`
 
+`;
+const HeaderImage = styled.div`
+  position: absolute;
+  z-index: -1;
+  height: 100vh;
+  width: 100vw;
+  background-image: url(background/header-hero.png);
+  background-size: cover;
+  background-position: left;
+`;
 const AppContainer = styled.div`
-background-color: pink;
+  font-family: Helvetica;
+  h1 {
+    font-family: 'Capture it';
+  }
 `;
