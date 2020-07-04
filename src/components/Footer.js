0c03672977d91bf96bbbd448149daa31045f9d13
Levi Winans <levieiko@gmail.com>
2020-07-04 16:11:53 -0400

    remove scroll prayer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ const Footer =()=> {
 };
 
 const Prayer = styled.div`
-margin-top: 67vh;
+
 height: 100vh;
 background-image: url(om-mani-padme-hum.png);
 background-size: 30%;
@@ -18,7 +18,7 @@ background-position: center;
 background-repeat: no-repeat;
 background-color: transparent;
 @media screen and (max-width: 900px) {
-    margin-top: 100vh;
+
     background-size: 80%;
     background-position: center 20%;
 }
