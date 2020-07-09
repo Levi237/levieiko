889886a68654971045838e35dbe353f2c9b854aa
Levi Winans <levieiko@gmail.com>
2020-07-08 20:30:22 -0700

    update

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ import * as firebase from "firebase/app";
 import 'firebase/firestore';
 
 firebase.initializeApp({
-  apiKey: "AIzaSyC5RR8DAz7z5QC97UPfsg61U5aCHlLNB0s",
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
   authDomain: "levieiko-com.firebaseapp.com",
   databaseURL: "https://levieiko-com.firebaseio.com",
   projectId: "levieiko-com",
   storageBucket: "levieiko-com.appspot.com",
   messagingSenderId: "560687419303",
-  appId: "1:560687419303:web:46b4771788242575dff082",
-  measurementId: "G-PSFLRM85XL"
+  appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
+  measurementId: `${process.env.REACT_APP_FIREBASE_MEASUREMENT_ID}`
 });
 export { firebase as default }
 
