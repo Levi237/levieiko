1b268563232e331d17a61036c0bfab291e6a8eb2
Levi Winans <levieiko@gmail.com>
2020-07-08 20:20:48 -0700

    reset initializeApp

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import * as firebase from "firebase/app";
-import 'firebase/database';
+import * as firebase from "firebase";
+import 'firebase/firestore';
 
 firebase.initializeApp({
-  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  apiKey: "AIzaSyC5RR8DAz7z5QC97UPfsg61U5aCHlLNB0s",
   authDomain: "levieiko-com.firebaseapp.com",
   databaseURL: "https://levieiko-com.firebaseio.com",
   projectId: "levieiko-com",
   storageBucket: "levieiko-com.appspot.com",
-  messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGE_ID}`,
-  appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
-  measurementId: `${process.env.REACT_APP_FIREBASE_MESSAGE_ID}`
+  messagingSenderId: "560687419303",
+  appId: "1:560687419303:web:46b4771788242575dff082",
+  measurementId: "G-PSFLRM85XL"
 });
 
 ReactDOM.render(
