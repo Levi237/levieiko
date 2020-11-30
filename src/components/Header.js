92bbdfe17ac6bb8c3700e44daf48a4ab9cd35d8f
Levi Winans <levieiko@gmail.com>
2020-11-30 15:43:48 -0800

    clean up code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,50 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Header =(props)=> {
+const Header =()=> {
     return(
         <Container>
-          <h1></h1>
-            {/* <h1>Levi Eiko Winans</h1> */}
-            {/* <section>
-              <span>+ 646 206 8777</span>
-              <br/>
-              <span>LeviEiko@gmail.com</span>
-            </section> */}
             <img src="logo.png" alt="levieiko.com"/>
-      </Container>
-        );
+        </Container>
+      );
 };
 
 const Container = styled.div`
-h1 {
-    font-size: 46px;
-    padding: 10px;
-  }
-  section {
-    text-align: right;
-
-    margin-top: -40px;
-    position: absolute;
-    right: 10px;
-  }
   img {
-    display: block;
-    margin: 30vh auto;
+    margin: calc(50vh - 60px) calc(50vw - 60px);
   }
-  @media screen and (max-width: 900px){
-    h1 {
-      text-align: center;
-    }
-    section {
-      font-size: 5vw;
-      font-weight: bold;
-      background-image: linear-gradient(transparent, rgba(0,0,0,.5));
-      padding: 5%;
-      width: 100%;
-      bottom: 0;
-      right: 0;
-    }
+  // @media screen and (max-width: 900px){
+  // }
 `;
 
 export default Header;
