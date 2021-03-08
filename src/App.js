d03fd4c71d4d73938af298dcd3c5f2475a087867
Levi Winans <levieiko@gmail.com>
2021-03-08 11:58:37 -0800

    fix color

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ export default class App extends Component {
       mail: {
         title: 'contact',
         color: '#fff',
-        fill: 'fff',
+        fill: '#fff',
         displayLogo: 'inline-block',
       },
       root: {
