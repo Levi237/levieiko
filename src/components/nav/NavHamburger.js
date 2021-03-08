b502396e2dc31f1ea8cd6fd086cdd6cb9911ace1
Levi Winans <levieiko@gmail.com>
2021-03-08 14:17:47 -0800

    replace hamburger css with svg

--- src/components/nav/NavHamburger.js
+++ src/components/nav/NavHamburger.js
@@ -3,7 +3,8 @@ import styled      from 'styled-components';
 import { NavLink } from 'react-router-dom';
 import * as routes from '../../constants/routes';
 
-import { ReactComponent as LogoSVG } from '../svg/logo.svg';
+import { ReactComponent as LogoSVG }      from '../../svg/logo.svg';
+import { ReactComponent as HamburgerSVG } from '../../svg/hamburger.svg';
 
 const NavHamburger = ({ toggleMenu, pageStyle }) => {
   return(<>
@@ -13,9 +14,7 @@ const NavHamburger = ({ toggleMenu, pageStyle }) => {
       </NavLink>
     </Logo>
     <Hamburger name="toggleMenu" onClick={(e) => toggleMenu(e)}>
-      <section style={{backgroundColor: pageStyle.color}}/>
-      <section style={{backgroundColor: pageStyle.color}}/>
-      <section style={{backgroundColor: pageStyle.color}}/>
+      <HamburgerSVG style={{fill: pageStyle.color}}/>
     </Hamburger>
   </>);
 };
@@ -24,6 +23,7 @@ const GlobalDiv = styled.div`
   position: absolute;
   z-index:999;
   top: 40px;
+  width: 36px;
   &:hover{
     cursor: pointer;
   }
@@ -33,15 +33,10 @@ const GlobalDiv = styled.div`
 `;
 const Logo = styled(GlobalDiv)`
   left: 5vw;
-  width: 36px;
 `;
 const Hamburger = styled(GlobalDiv)`
   right: 5vw;
-  section {
-    margin: 6px 0;
-    width: 35px;
-    height: 4px;
-  }
+
 `;
 
 export default NavHamburger;
