a4603feb96f92c58f26dbd33b8454d08284d1095
Levi Winans <levieiko@gmail.com>
2021-03-13 12:01:11 -0800

    arrange skills

--- src/components/clients/index.js
+++ src/components/clients/index.js
@@ -17,39 +17,40 @@ export default class PortfolioPage extends Component {
             'Git',
             'Shopify',
             'WordPress',
+            'Figma',
             'MailChimp',
             'IDX Broker',
-            'PhotoShop',
+            'Adobe PhotoShop',
             'Liquid',
-            'CSS',
             'HTML',
-            'JavaScript',
-            'VanillaJS'
+            'CSS',
+            'VanillaJS',
+            'JavaScript'
         ],
         projects: [{
             title: 'PokeGen',
             image: './projects/pokegen.png',
             link: 'https://pokegen.herokuapp.com/',
             lineOne: 'Build your own Pokemon Card MEME.',
-            lineTwo:'Login, create, and share.'
+            lineTwo: 'MongoDB, Express, NodeJS [MEN]'
         },{
-            title: 'Hi Sierra!',
-            image: './projects/hi-sierra.png',
-            link: 'https://parkintel.herokuapp.com/home',
-            lineOne: 'Learn more about the National Parks.',
-            lineTwo:'Track unexpected closures.'
-        },{
-            title: 'LA-HOP',
-            image: './projects/la-hop.png',
-            link: 'https://lahsa-la-hopv2.herokuapp.com/request',
-            lineOne: 'Tools for the public to help',
-            lineTwo: 'report homeless needs.'
-        },{
+        //     title: 'Hi Sierra!',
+        //     image: './projects/hi-sierra.png',
+        //     link: 'https://parkintel.herokuapp.com/home',
+        //     lineOne: 'Learn more about the National Parks.',
+        //     lineTwo:'Track unexpected closures.'
+        // },{
+        //     title: 'LA-HOP',
+        //     image: './projects/la-hop.png',
+        //     link: 'https://lahsa-la-hopv2.herokuapp.com/request',
+        //     lineOne: 'Tools for the public to help',
+        //     lineTwo: 'report homeless needs.'
+        // },{
             title: 'BlackJack',
             image: './projects/blackjack.png',
             link: 'https://levi237.github.io/blackjack-reup/',
-            lineOne: 'Bid, Hit, Double Down, Call.',
-            lineTwo: 'Black Jack circa 1990.'
+            lineOne: 'Black Jack circa 1990.',
+            lineTwo: 'Javascript, VanillaJS, HTML5, CSS3'
         }]
     }
     render(){
