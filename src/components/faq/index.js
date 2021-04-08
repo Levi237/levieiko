9458620269e21b7be9adee651632ead7eb86c494
Levi Winans <levieiko@gmail.com>
2021-04-08 09:36:34 -0700

    style text

--- src/components/faq/index.js
+++ src/components/faq/index.js
@@ -9,7 +9,7 @@ import { ReactComponent as Linkedin }   from '../../svg/icons/iconfinder_linkedi
 import { ReactComponent as Github }     from '../../svg/icons/iconfinder_github.svg';
 
 
-const FAQ = () => { 
+const FAQPage = () => { 
   return(
     <Container>
         <h1 className="titleDecoration">FAQ</h1>
@@ -17,32 +17,45 @@ const FAQ = () => {
           <section>
             <h2>Mobile-first website OR an APP?</h2>
             <p>
-              It really depends on the complexity.  Apps sound glamorous but there are a few things to consider.  Apps are only intended for mobile and tablets.  They need to be downloaded and take up space on peoples phones.  Most businesses can create the same experience with a mobile-first designed website with added versatility.
+              It really depends on the complexity.  
+              Apps sound glamorous but there are a few things to consider.  
+              Apps are only intended for mobile and tablets.  
+              They need to be downloaded and take up space on peoples phones.
+              <br/>
+              Most businesses can create the same experience with a mobile-first designed website with added versatility.
             </p>
           </section>
           <section>
             <h2>How much does a website cost?</h2>
-            <p>Certain expenses vary, like hosting, domain name, SSL, email, thid party integrations and generally have monthly or annual expenses.
-            A s
-            mall business website can run anywere from $500 for a single page portfolio to $10,000 high-end eCommerce boutique.  You can always start small and build up.  The sooner you start generating revenue the sooner you can invest in yourself!
+            <p>
+              Certain expenses vary, like hosting, domain name, SSL, email, thid party integrations and generally have monthly or annual expenses.
+              <br/>
+              A small business website can run anywere from $500 for a single page portfolio to $10,000 high-end eCommerce boutique.  
+              You can always start small and build up.  
+              The sooner you start generating revenue the sooner you can invest in yourself!
             </p>
           </section>
           <section>
             <h2>What kind of web developer is Levi?</h2>
             <p>
-              I am a freelance full-stack web developer with email marketing and graphic design skills.  AKA a “ one man band.”  I can code your website or customize a third party hosting service like Shopify, WordPress, etc, and integrate third party technologies.  All with style 😉
+              I am a freelance full-stack <b>web developer</b> with <b>email marketing</b> and <b>graphic design</b> skills.  
+              AKA a “ one man band.”  
+              I can code your website or customize a third party hosting service like Shopify, WordPress, etc.  
+              All with style 😉
             </p>
           </section>
           <section>
             <h2>What are alternatives to hiring a freelancer?</h2>
             <p>
-              There are agencies that work in teams that can provide more services like copy writing, data analysis, surveys, etc.  Or you can DIY with WIX, SquareSpace, Weebly, etc... just make sure to uncheck those additional services boxes that may incur future charges.  
+              There are agencies that work in teams that can provide more services like copy writing, data analysis, surveys, etc.  
+              Or you can DIY with WIX, SquareSpace, Weebly, etc... make sure to uncheck those additional services boxes that may incur future charges.
             </p>
           </section>
           <section>
             <h2>What if I have a great product or brand but I can’t afford the price?</h2>
             <p>
-              There are certain times when my gut tells me ‘this client is a good investment’ and I’m usually right.  If you have someting started and see real potential in upping your game, I am down to chat.
+              There are certain times when my gut tells me ‘this client is a good investment’ and I’m usually right.  
+              If you have someting started and see real potential in upping your game, I am down to chat.
             </p>
           </section>
           <section>
@@ -76,10 +89,27 @@ const FAQ = () => {
 };
 
 const Container = styled.div`
-  background-color: rgba(19,17,17,.8);
+  background-color: #fff;
+  color: #181717;
   padding: 80px 0;
   margin: 0 auto!important;
-
+  div {
+    margin: 60px auto;
+    width: 70%;
+    max-width: 800px;
+  }
+  section {
+    margin-bottom: 60px;
+  }
+  ul {
+    line-height: 150%;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+  }
+  svg {
+    height: 40px;
+    width: 40px;
+  }
   @media screen and (max-width: 745px){
       padding-top: 60px;
       > h1 {
@@ -88,4 +118,4 @@ const Container = styled.div`
   }
 `;
 
-export default FAQ;
+export default FAQPage;
