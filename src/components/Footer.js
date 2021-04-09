ba2e403ff915737b11885f3e649a13454fd68789
Levi Winans <levieiko@gmail.com>
2021-04-09 14:40:49 -0700

    adjust footer for mobile and desktop

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -58,28 +58,83 @@ const Footer = () => {
 const Container = styled.footer`
   width: 100vw;
   text-align: center;
+  background-color: darkgrey;
   > div {
     width: 100vw;
     > section {
+      width: 27vw;
+      padding: 3vw;
       display: inline-block;
       &:first-of-type {
-        width: calc(33vw - 160px);
         text-align: left;
+        vertical-align: top;
       }
       &:nth-of-type(2) {
-        width: 33vw;
-        min-width: 320px;
-
+        vertical-align: middle;
       }
       &:last-of-type {
-        width: calc(33vw - 160px);
         text-align: right;
+        vertical-align: bottom;
       }
     }
     svg {
       height: 40px;
       width: 40px;
-      margin: 10px;
+      margin: 15x;
+      fill: white;
+    }
+    a {
+      color: #fff;
+      padding: 0 10px;
+    }
+  }
+  @media screen and (max-width: 900px){
+    > div {
+      > section {
+        width: calc(22vw - 160px);
+        padding: 3vw;
+        display: inline-block;
+        &:first-of-type {
+          text-align: left;
+          padding: 3vw;
+        }
+        &:nth-of-type(2) {
+          width: 320px;
+          padding: 0 3vw;
+        }
+        &:last-of-type {
+          text-align: right;
+          padding: 3vw;
+        }
+      }
+      svg {
+        height: 40px;
+        width: 40px;
+        margin: 10px;
+      }
+    }
+  }
+    @media screen and (max-width: 745px){
+      > div {
+        > section {
+          width: 27vw;
+          padding: 3vw;
+          display: inline-block;
+          &:first-of-type {
+            text-align: left;
+          }
+          &:nth-of-type(2) {
+          }
+          &:last-of-type {
+            text-align: right;
+          }
+        }
+        svg {
+          height: 40px;
+          width: 40px;
+          margin: 10px;
+        }
+      }
     }
   }
 `;
