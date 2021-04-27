3ba8ca96b636b3e42b5305ee22084135eefcd332
Levi Winans <levieiko@gmail.com>
2021-04-27 02:07:48 -0700

    add mailchimp component with image / bullets

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,9 +1,12 @@
-import React, { Component }     from 'react';
-import styled      from 'styled-components';
-import { NavLink } from 'react-router-dom';
-import * as routes from '../../constants/routes';
+import React, { Component } from 'react';
+import styled               from 'styled-components';
+import { NavLink }          from 'react-router-dom';
+import * as routes          from '../../constants/routes';
 
-import Footer      from '../Footer';
+import MailChimpBanner      from '../MailChimpBanner';
+import WireframeBanner      from '../WireframeBanner';
+import InfoBanner           from '../InfoBanner';
+import Footer               from '../Footer';
 
 import { ReactComponent as LogoSVG } from '../../svg/brand/logo.svg';
 
@@ -11,49 +14,29 @@ export default class HomePage extends Component {
   render(){
 
     const { pageStyle, btnScroll } = this.props
-  return(
-    <HomeWrapper>
-      <HomeHeroWrapper>
-        <NavLink to={routes.PORT}>
-          <LogoSVG className="" style={{fill: pageStyle.fill, display: pageStyle.display}}/>
-        </NavLink>
-        <div id="down-arrow-container" onClick={() => btnScroll()}>
-          <div id="down-arrow"></div>
-        </div>
-      </HomeHeroWrapper>
-        <HomeSecondNav id="scroll-to">
-          <h1>LEVIEIKO.COM</h1>
-        </HomeSecondNav>
-      <HomeBodyWrapper>
-        <br/><br/><br/>
-        <InfoBox>
-          <section>
-            <div></div>
-            <img src="/pranawellnessworks-mobile-small.png"/>
-            <section></section>
-          </section>
-          <section>
-            <div>
-              <h1>MOBILE-FIRST</h1>
-              <h3>You actually <b>don't</b> need an app for that.</h3>
-              <p>
-                For most people their primary device is their cell phone.
-                With a mobile-first website you can create that same app experience 
-                without forcing users to download more stuff onto their phones or 
-                switching between iPhone and Android devices.
-              </p>
-              <p>
-                Welcome to the future!
-              </p>
-            </div>
-          </section>
-          <div></div>
-        </InfoBox>
-        <Footer/>
-      </HomeBodyWrapper>
-    </HomeWrapper>
-  );
-};
+
+    return(
+      <HomeWrapper>
+        <HomeHeroWrapper>
+          <NavLink to={routes.PORT}>
+            <LogoSVG className="" style={{fill: pageStyle.fill, display: pageStyle.display}}/>
+          </NavLink>
+          <div id="down-arrow-container" onClick={() => btnScroll()}>
+            <div id="down-arrow"></div>
+          </div>
+        </HomeHeroWrapper>
+          <HomeSecondNav id="scroll-to">
+            <h1>LEVIEIKO.COM</h1>
+          </HomeSecondNav>
+        <HomeBodyWrapper>          
+          <InfoBanner/>
+          <WireframeBanner/>
+          <MailChimpBanner/>
+          <Footer/>
+        </HomeBodyWrapper>
+      </HomeWrapper>
+    );
+  };
 }
 
 const HomeWrapper = styled.div`
@@ -63,12 +46,15 @@ const HomeWrapper = styled.div`
   overflow: scroll;
 `;
 const HomeSecondNav = styled.nav`
-  height: calc(62px + 5vw);
+  height: calc(50px + 5vw);
   z-index: 2;
   background-color: var(--darkblue);
+  -webkit-scroll-snap-align: start;
+  -moz-scroll-snap-align: start;
+  -ms-scroll-snap-align: start;
   scroll-snap-align: start;
   > h1 {
-    margin: 5vw!important;
+    margin: calc(5vw + 2px)!important;
     display: inline-block;
     font-size: 32px;
     line-height: 100%;
@@ -79,15 +65,19 @@ const HomeSecondNav = styled.nav`
       margin: 40px!important;
     }
   }
-
 `;
 
 const HomeHeroWrapper = styled.div`
   height: 100vh;
   width: 100vw;
+  -webkit-scroll-snap-align: start;
+  -moz-scroll-snap-align: start;
+  -ms-scroll-snap-align: start;
   scroll-snap-align: start;
   display: flex;
+  -webkit-box-align: center;
   align-items: center;
+  -webkit-box-pack: center;
   justify-content: center;
   overflow: scroll;
   > a {
@@ -99,9 +89,11 @@ const HomeHeroWrapper = styled.div`
 const HomeBodyWrapper = styled.div`
   position: relative;
   width: 100vw;
-  height: calc(100vh - 5vw - 62px);
+  height: calc(100vh - 5vw - 50px);
   background-color: #fff;
   overflow: scroll;
+  padding-top: 20px;
+  scroll-snap-type: y mandatory;
   > h1 {
     color: var(--darkblue);
     font-size: 32px;
@@ -111,185 +103,5 @@ const HomeBodyWrapper = styled.div`
   }
   @media screen and (min-width: 945px) {
     height: calc(100vh - 112px);
-  }
-`;
-const InfoBox = styled.section`
-  position: relative;
-  max-width: 100vw;
-  height: 100%;
-  margin: 0 auto;
-
-  > div {
-    position: absolute;
-    width: 80vw;
-    height: 80vw;
-    border: 1px solid black;
-    z-index: 0;
-    top: 75vw;
-    left: calc(10vw - 1px);
-  }
-
-  > section {
-    position: relative;
-    height: 90vw;
-    width: 90vw;
-    border: none;
-    font-size: 0;
-    z-index: 1;
-    color: var(--darkgrey);
-
-    &:first-of-type {
-      margin: 5vw 0 0 5vw;
-
-      > div {
-        height: 85%;
-        width: 85%;
-        position: absolute;
-        left: 0;
-        top: 0;
-        background-image: url(https://pranawellnessworks.com/home-meet.png);
-        background-size: cover;
-      }
-      > img {
-        width: 36%;
-        position: absolute;
-        right: 0;
-        bottom: 0;
-      }
-    }
-    &:last-of-type {
-      margin: 0 0 5vw 5vw;
-      display: flex;
-      align-items: start;
-      justify-content: center;
-
-      > div {
-        width: 100%;
-        display: inline-block;
-        vertical-align: middle;
-        padding: 0% 15% 0% 10%;
-      }
-      h1 {
-        color: var(--black);
-        font-size: 8vw;
-        margin: 0% 0% 5%!important;
-        text-align: left;
-      }
-      h3 {
-        font-size: 4vw;
-        font-weight: 500;
-        font-style: italic;
-        margin: 0!important;
-      }
-      p {
-        font-size: 3.5vw;
-        &:last-of-type {
-          margin-bottom: 0!important;
-          font-weight: bold;
-        }
-      }
-    }
-  }
-  @media screen and (min-width: 640px) {
-    max-width: 1000px;
-    height: 50vw;
-    max-height: 500px;
-    margin: 0 auto;
-    font-size: 0; 
-    display: flex;
-    align-items: center;  
-    justify-content: center; 
-    > div {
-      position: absolute;
-      width: 70vw;
-      height: 35vw;
-      max-width: 700px;
-      max-height: 350px;
-      border: 1px solid black;
-      z-index: 0;
-      
-      top: 7.5vw;
-      left: 15vw;
-    }
-    > section {
-      position: relative;
-      height: 80%;
-      width: 40%;
-      border: none;
-      font-size: 0;
-      z-index: 1;
-      color: var(--darkgrey);
-      margin: 0!important;
-      &:last-of-type {
-        
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        > div {
-          width: 100%;
-          display: inline-block;
-          vertical-align: middle;
-          padding: 0% 15% 0% 10%;
-        }
-        h1 {
-          color: var(--black);
-          font-size: 4vw;
-          margin: 0% 0% 5%!important;
-          text-align: left;
-        }
-        h3 {
-          font-size: 1.8vw;
-        }
-        p {
-          font-size: 1.5vw;
-          &:last-of-type {
-            margin-bottom: 0!important;
-          }
-        }
-      }
-    }
-  }
-  @media screen and (min-width: 1000px) {
-    > div {
-      top: 75px;
-      left: 150px;
-    }
-    > section {
-      position: relative;
-      height: 80%;
-      width: 40%;
-      border: none;
-      font-size: 0;
-      z-index: 1;
-      color: var(--darkgrey);
-
-      &:last-of-type {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        > div {
-          width: 100%;
-          display: inline-block;
-          vertical-align: middle;
-          padding: 0% 15% 0% 10%;
-        }
-        h1 {
-          color: var(--black);
-          font-size: 32px;
-          margin: 0% 0% 5%!important;
-          text-align: left;
-        }
-        h3 {
-          font-size: 17px;
-          font-weight: 500;
-          font-style: italic;
-      
-          margin: 0!important;
-        }
-        p {
-          font-size: 15px;
-        }
-      }
-    }
   }
 `;
